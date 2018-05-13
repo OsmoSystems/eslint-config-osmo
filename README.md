b4eb9cc7a46c6e11ee017055fac95ea28c2a5662
Jaime <jaime@osmobot.com>
2018-05-13 16:32:25 -0700

    Initial eslintrc.js for testing

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ An [ESLint][eslint] [config file][config] for all Osmo Systems Javascript code.
 Install this package as a devDependency:
 
     npm install --save-dev eslint@3.7 babel-eslint@7.0 eslint-plugin-import@2.0 eslint-plugin-react@7.0 eslint-plugin-unicorn@2.1
-    npm install --save-dev "git+ssh://git@github.com/OsmoSystems/eslint-config-osmo.git",
+    npm install --save-dev "git+ssh://git@github.com/OsmoSystems/eslint-config-osmo.git"
 
 
 ## Configuration
---
+++ eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    // "plugins": [
+    //     "unicorn",
+    //     "import",
+    //     "react"
+    // ],
+    // "env": {
+    //     "es6": true,
+    //     "jasmine": true,
+    //     "node": true
+    // },
+    // "parserOptions": {
+    //     "ecmaFeatures": {
+    //         "jsx": true
+    //     },
+    //     "ecmaVersion": 6,
+    //     "sourceType": "module"
+    // },
+    "extends": "airbnb",
+    "rules": {
+        "indent": [2, 4],
+        "react/jsx-indent": [2, 4],
+        "react/jsx-indent-props": [2, 4]
+    }
+};
