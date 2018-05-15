fceeea4232c2b9295f3f6d3279fd6f7b26001c00
Jaime <jaime@osmobot.com>
2018-05-15 09:34:34 -0700

    Make some lint rules warnings to avoid boiling ocean

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ module.exports = {
     },
     "rules": {
         "indent": [2, 4],
-        "max-len": [1, { "code": 100 }]
+        "max-len": [1, { "code": 100 }],
+        "comma-dangle": [2, {
+            "arrays": "always-multiline",
+            "objects": "always-multiline",
+            "imports": "always-multiline",
+            "exports": "always-multiline",
+            "functions": "never",
+        }],
+        "consistent-return": [1],
+        // TODO (SOFT-49): These are warnings for now (to avoid boiling ocean). Make them errors.
+        "camelcase": [1],
+        "no-param-reassign": [1]
     }
 };
