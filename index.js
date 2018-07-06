f49cad46fb8a918851a4ec51e874b883732e0a7b
Jaime <jaime@osmobot.com>
2018-07-06 16:14:34 -0700

    Make `function-paren-newline` rule more permissive

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ module.exports = {
         "ecmaVersion": 2017,
     },
     "rules": {
-        "indent": [2, 4],
-        "max-len": [1, { "code": 120 }],
+        "camelcase": [1], // TODO (SOFT-49): Warning for now (to avoid boiling ocean). Make it an error.
         "comma-dangle": [2, {
             "arrays": "always-multiline",
-            "objects": "always-multiline",
-            "imports": "always-multiline",
             "exports": "always-multiline",
             "functions": "never",
+            "imports": "always-multiline",
+            "objects": "always-multiline",
         }],
         "consistent-return": [1],
+        "function-paren-newline": ["error", "consistent"],
+        "indent": [2, 4],
+        "max-len": [1, { "code": 120 }],
+        "no-param-reassign": [1], // TODO (SOFT-49): Warning for now (to avoid boiling ocean). Make it an error.
         "object-curly-newline": [2, { "consistent": true }],
-        // TODO (SOFT-49): These are warnings for now (to avoid boiling ocean). Make them errors.
-        "camelcase": [1],
-        "no-param-reassign": [1]
     }
 };
